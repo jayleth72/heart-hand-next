f4715adac0dfd3a4ffd569c65c110472620679dc
jayleth72 <jay.letheby@gmail.com>
2024-01-17 20:13:43 +1000

    added socia icon, links in constants folder

---
+++ src/app/constants/social-icons.js
@@ -0,0 +1,19 @@
+import { FaFacebook, FaTwitterSquare, FaInstagramSquare, FaYoutube } from "react-icons/fa"
+export default [
+  {
+    icon: <FaFacebook />,
+    url: "https://www.facebook.com/linda.letheby.9",
+  },
+  {
+    icon: <FaTwitterSquare />,
+    url: "https://twitter.com/lees_Art",
+  },
+  {
+    icon: <FaInstagramSquare />,
+    url: "https://www.instagram.com/gardenhomeschool/",
+  },
+  {
+    icon: <FaYoutube />,
+    url: "https://www.youtube.com/channel/UCxjuQsvfmTE7DIwCdcdPc_A",
+  },
+]
